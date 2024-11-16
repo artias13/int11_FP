f20cc448270a0d1187bf65aab3a2c3cc1e18f2c2
artias13 <archee.busy@gmail.com>
2024-11-16 16:09:15 +0800

    upd

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ S3_ENDPOINT = os.getenv('S3_ENDPOINT')
 S3_PORT = os.getenv('S3_PORT')
 S3_ACCESS_KEY = os.getenv('S3_ACCESS_KEY')
 S3_SECRET_KEY = os.getenv('S3_SECRET_KEY')
-S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
 
 def main():
     logger.info("vx-underground FP started")
@@ -53,6 +52,7 @@ def main():
     process_samples( os.path.join(os.getcwd(), "tmp"), os.path.join(os.getcwd(), "rules"))
     
     try:
+        S3_BUCKET_NAME = f"{target_date.strftime('%Y')}-{target_date.strftime('%m')}-{target_date.strftime('%d')}" + "_vx-underground"
         minio_client.create_bucket(S3_BUCKET_NAME)
         # Загружаем файлы в бакет
         for file in os.listdir(os.path.join(os.getcwd(), "tmp")):
